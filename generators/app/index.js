24851b8f2b031361a64b041443b83ab92c0ee2c5
guid21 <uryuk.uz@gmail.com>
2021-07-18 17:15:04 +0300

    Added redux endpoint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,45 +1,94 @@
-const Generator = require("yeoman-generator");
+const Generator = require('yeoman-generator');
+
+function capitalize(s) {
+  return s[0].toUpperCase() + s.slice(1);
+}
 
 module.exports = class extends Generator {
-  prompting() {
-    const prompts = [
+  async prompting() {
+    await this.prompt([
       {
-        type: "input",
-        name: "name",
-        message: "Your component name"
+        type: 'list',
+        choices: ['component', 'redux'],
+        name: 'module',
+        message: 'What module do you want to build?',
       },
-      {
-        type: "confirm",
-        name: "styles",
-        message: "Would you like to create SCSS Module?"
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      this.name = props.name;
-      this.styles = props.styles;
+    ]).then(({ module }) => {
+      this.module = module;
     });
+
+    switch (this.module) {
+      case 'component':
+        return this.prompt([
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Your component name',
+          },
+        ]).then((props) => {
+          this.name = props.name;
+        });
+      case 'redux':
+        return this.prompt([
+          {
+            type: 'input',
+            name: 'name',
+            message: 'Your redux endpoint name',
+          },
+        ]).then((props) => {
+          this.name = props.name;
+        });
+    }
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath("component.html"),
-      this.destinationPath(`${this.name}/${this.name}.tsx`),
-      { title: this.name }
-    );
+    switch (this.module) {
+      case 'component':
+        this.fs.copyTpl(
+          this.templatePath('component/index.html'),
+          this.destinationPath(`${this.name}/index.ts`),
+          { title: capitalize(this.name) }
+        );
 
-    this.fs.copyTpl(
-      this.templatePath("index.html"),
-      this.destinationPath(`${this.name}/index.ts`),
-      { title: this.name }
-    );
+        this.fs.copyTpl(
+          this.templatePath('component/component.html'),
+          this.destinationPath(`${this.name}/${this.name}.tsx`),
+          { title: capitalize(this.name) }
+        );
 
-    if (this.styles) {
-      this.fs.copyTpl(
-        this.templatePath("styles.html"),
-        this.destinationPath(`${this.name}/${this.name}.module.less`),
-        { title: this.name }
-      );
+        this.fs.copyTpl(
+          this.templatePath('component/styles.html'),
+          this.destinationPath(`${this.name}/${this.name}.module.less`),
+          { title: capitalize(this.name) }
+        );
+        break;
+      case 'redux':
+        this.fs.copyTpl(
+          this.templatePath('redux/index.html'),
+          this.destinationPath(`${this.name}/index.ts`),
+          { title: this.name }
+        );
+        this.fs.copyTpl(
+          this.templatePath('redux/endPoint.actions.html'),
+          this.destinationPath(`${this.name}/${this.name}.actions.ts`),
+          { titleWithCapitalise: capitalize(this.name) }
+        );
+        this.fs.copyTpl(
+          this.templatePath('redux/endPoint.service.html'),
+          this.destinationPath(`${this.name}/${this.name}.service.ts`),
+          { title: this.name, titleWithCapitalise: capitalize(this.name) }
+        );
+        this.fs.copyTpl(
+          this.templatePath('redux/endPoint.effect.html'),
+          this.destinationPath(`${this.name}/${this.name}.effect.ts`),
+          { title: this.name, titleWithCapitalise: capitalize(this.name) }
+        );
+        this.fs.copyTpl(
+          this.templatePath('redux/endPoint.reducer.html'),
+          this.destinationPath(`${this.name}/${this.name}.reducer.ts`),
+          { title: this.name, titleWithCapitalise: capitalize(this.name) }
+        );
+        break;
     }
   }
 
